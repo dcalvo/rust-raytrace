345dbe3f75db5f46654c8eec48b409aab29720ce
David Calvo <david@dcalvo.io>
2023-03-11 01:13:50 -0500

    add vector math crate

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use image::{ImageBuffer, RgbImage};
+use glam::{vec3, Vec3};
+use image::{ImageBuffer, Rgb, RgbImage};
 use std::ops::{Add, Div, Mul, Sub};
+
+type Point3 = Vec3;
+type Color = Vec3;
 
 fn map_range<T: Copy>(s: T, from_range: (T, T), to_range: (T, T)) -> T
 where
     T: Add<T, Output = T> + Sub<T, Output = T> + Mul<T, Output = T> + Div<T, Output = T>,
 {
     to_range.0 + (s - from_range.0) * (to_range.1 - to_range.0) / (from_range.1 - from_range.0)
+}
+
+fn vec_to_pixel(vec: Color) -> Rgb<u8> {
+    let r = map_range(vec.x, (0.0, 1.0), (0.0, 255.999)) as u8;
+    let g = map_range(vec.y, (0.0, 1.0), (0.0, 255.999)) as u8;
+    let b = map_range(vec.z, (0.0, 1.0), (0.0, 255.999)) as u8;
+
+    image::Rgb([r, g, b])
 }
 
 fn main() {
@@ -22,14 +34,11 @@ fn main() {
             let g = y as f32 / (image_height - 1) as f32;
             let b = 0.25;
 
-            let ir = map_range(r, (0.0, 1.0), (0.0, 255.999)) as u8;
-            let ig = map_range(g, (0.0, 1.0), (0.0, 255.999)) as u8;
-            let ib = map_range(b, (0.0, 1.0), (0.0, 255.999)) as u8;
+            let vec: Color = vec3(r, g, b);
 
             // ImageBuffer's origin is top-left, or (0, image_height), so subtract y to move it
             let pixel = image_buffer.get_pixel_mut(x, (image_height - y) - 1);
-            *pixel = image::Rgb([ir, ig, ib]);
-            // println!("{ir} {ig} {ib}");
+            *pixel = vec_to_pixel(vec);
         }
     }
 
